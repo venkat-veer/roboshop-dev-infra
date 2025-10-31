d9f0f7543d6204113b4bfb1205939f7ca6ad98af
venkat-veer <s.venkatveer@gmail.com>
2025-10-31 08:24:08 +0530

    mongodb-bastion

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,4 +1,5 @@
 locals {
+    ami_id  = data.aws_ami.joindevops.id
     common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     database_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_ids.value)[0]

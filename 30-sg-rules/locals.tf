e25e3b2ccba7dcc962f5bd7fa647dea4b6a8191a
venkat-veer <s.venkatveer@gmail.com>
2025-11-01 00:53:25 +0530

    sg-rules

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -3,4 +3,6 @@ locals {
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
     mangodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+    rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
 }

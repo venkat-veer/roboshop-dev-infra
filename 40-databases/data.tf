8e6fde6bb83469639312d005495e4f18db3b382f
venkat-veer <s.venkatveer@gmail.com>
2025-11-01 00:54:27 +0530

    rabbitmq-mysql

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -26,6 +26,14 @@ data "aws_ssm_parameter" "redis_sg_id" {
     name = "/${var.project_name}/${var.environment}/redis_sg_id"
 }
 
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+    name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id"
+}
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+    name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+}
+
 data "aws_ssm_parameter" "database_subnet_ids" {
     name = "/${var.project_name}/${var.environment}/database_subnet_ids"
 }
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -75,3 +75,42 @@ resource "terraform_data" "redis" {
 }
 
 
+# rabbitmq database configure
+resource "aws_instance" "rabbitmq" {
+    ami = local.ami_id      
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.rabbitmq_sg_id]
+    subnet_id = local.database_subnet_id
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-rabbitmq"        # roboshop-dev-mongodb  
+        }
+    )
+}
+resource "terraform_data" "rabbitmq" {
+    triggers_replace = [aws_instance.rabbitmq.id]
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = aws_instance.rabbitmq.private_ip 
+    }
+
+    # terrform copies this file to mongodb server   
+    provisioner "file" {
+        source = "bootstrap.sh"
+        destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec" {
+        inline = [ 
+            "chmod +x /tmp/bootstrap.sh",
+            "sudo sh /tmp/bootstrap.sh rabbitmq"
+            ]
+      
+    }
+}
+
+

a6c98ab9dae1c60585c0f6cbaedcb98bd3af3bc6
venkat-veer <s.venkatveer@gmail.com>
2025-11-03 11:05:13 +0530

    db-change

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,8 +1,33 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/venkat-veer/ansible-roboshop-roles-tf.git -e component=$component main.yml
+# ansible-pull -U https://github.com/venkat-veer/ansible-roboshop-roles-tf.git -e component=$component main.yml
 
 # git clone ansible-playbook
 # cd ansible-playbook
 # ansible-playbook -i inventory main.yml
+
+
+REPO_URL = https://github.com/venkat-veer/ansible-roboshop-roles-tf.git
+REPO_DIR = /opt/roboshop/ansible
+ANSIBLE_DIR = ansible-roboshop-roles-tf
+
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
+
+cd $REPO_DIR
+
+# check if ansible repo is already cloned or not
+ 
+if [ -d $ANSIBLE_DIR ]; then
+    cd $ANSIBLE_DIR
+    git pull
+
+else
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+
+ansible-playbook -e compnent=$component main.yml

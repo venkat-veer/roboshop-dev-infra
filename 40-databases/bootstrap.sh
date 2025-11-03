dc6eb40943009ed1d495d12c7db07ca8fca1807a
venkat-veer <s.venkatveer@gmail.com>
2025-11-03 10:06:00 +0530

    bootstrap-file

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,3 +2,7 @@
 component=$1
 dnf install ansible -y
 ansible-pull -U https://github.com/venkat-veer/ansible-roboshop-roles-tf.git -e component=$component main.yml
+
+# git clone ansible-playbook
+# cd ansible-playbook
+# ansible-playbook -i inventory main.yml

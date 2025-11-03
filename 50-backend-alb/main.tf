49d15d0888931cc53891eea2e40372b50e4fc8bd
venkat-veer <s.venkatveer@gmail.com>
2025-11-03 10:02:45 +0530

    load-balancer-fasle

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -7,8 +7,8 @@ resource "aws_lb" "backend_alb" {
     # it should be private subnet ids
     subnets = local.private_subnet_ids
 
-    enable_deletion_protection = true                    # prevents accidental deletion from UI
-
+    # enable_deletion_protection = true                    # prevents accidental deletion from UI
+      enable_deletion_protection = false
     tags = merge (
         local.common_tags,
         {

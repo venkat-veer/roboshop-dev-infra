67c4526ac488c64781c5e15bc2fb299c97432ed3
venkat-veer <s.venkatveer@gmail.com>
2025-11-04 18:46:13 +0530

    dependson-ami-catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -53,4 +53,10 @@ resource "aws_ami_from_instance" "catalogue" {
     name = "${local.common_name_suffix}-catalogue-ami"
     source_instance_id = aws_instance.catalogue.id
     depends_on = [aws_ec2_instance_state.catalogue]
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-catalogue"        # roboshop-dev-catalogue  
+        }
+    )
 }

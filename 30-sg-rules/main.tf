f54d0f8b08f677ed6120fdc0d49f295a9f97133e
venkat-veer <s.venkatveer@gmail.com>
2025-11-06 18:50:06 +0530

    rule-add-catalogue

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -73,3 +73,13 @@ resource "aws_security_group_rule" "mongodb_catalogue" {
     protocol = "tcp"
     to_port = 27017
 }
+
+# catalogue instance accept traffic from backend load balancer
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+    type = "ingress"                                           # inbound rule
+    security_group_id =  local.catalogue_sg_id
+    source_security_group_id = local.backend_alb_sg_id 
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+}
